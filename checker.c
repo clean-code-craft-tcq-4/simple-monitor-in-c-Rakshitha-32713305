530f9d70bfeb36aa27b69ec7a3023886845a7e7e
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 20:17:19 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -4,15 +4,6 @@
 
 int main() 
 { 
-  assert(batteryIsOk(0, 20, 0.8));
-  printf("************************************\n"); 
-  assert(batteryIsOk(45, 70, 0.8));
-  printf("************************************\n"); 
- assert(!batteryIsOk(50, 85, 0));
-  printf("************************************\n"); 
- assert(batteryIsOk(5, 24, 0.1));
-  printf("************************************\n"); 
- assert(batteryIsOk(43, 80, 0.7));
-  printf("************************************\n"); 
+
  assert(batteryIsOk(111, 70, 0.7));
 }
