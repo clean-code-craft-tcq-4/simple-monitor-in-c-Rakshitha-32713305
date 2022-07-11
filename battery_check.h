b8e0281f15a999ca869b98f9952b09ae1083f13c
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 20:32:13 +0530

    Update battery_check.h

--- battery_check.h
+++ battery_check.h
@@ -2,4 +2,4 @@
 #include <stdio.h>
 
 extern int is_batteryparam_ok (int ret_temp,int ret_soc,int ret_CR);
-extern int batteryIsOk(float temperature, float soc, float chargeRate, bool conversion);
+extern int batteryIsOk(float temperature, float soc, float chargeRate,boolean conversion);
