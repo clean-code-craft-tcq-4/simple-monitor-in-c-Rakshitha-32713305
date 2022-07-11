3b25a8986a0a6855c0ce7f289ae2fea86629d041
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 20:42:26 +0530

    Update parameters_incommonunits.c

--- parameters_incommonunits.c
+++ parameters_incommonunits.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-void tempInCelcius(float *temperature)
+float tempInCelcius(float temperature)
 {  
- *temperature = ((*temperature) - 32) * 5 / 9; 
-  printf("temp %f in celsius\n",*temperature);
+  float temp =0;
+  temp = ((temperature) - 32) * 5 / 9; 
+  printf("temp %f in celsius\n",temp);
+  return temp;
 }
