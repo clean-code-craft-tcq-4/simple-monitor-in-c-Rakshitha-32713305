17310d6fd5699f4e9b3f21e5d1934be40dc2ca85
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 17:43:57 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,4 +7,6 @@ int main()
   assert(batteryIsOk(0, 20, 0.8));
   assert(batteryIsOk(45, 80, 0.8));
   assert(!batteryIsOk(50, 85, 0));
+  assert(batteryIsOk(5, 24, 0.1));
+  assert(batteryIsOk(43, 79, 0.7));
 }
