574c10b6973e723edf5b9a46f4512fd3646fc7f7
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 18:24:43 +0530

    Update batteryparameter_check.c

--- batteryparameter_check.c
+++ batteryparameter_check.c
@@ -2,6 +2,25 @@
 #include "battery_range_tolerance_handling.h"
 include <stdio.h>
 
+
+//parameter ranges
+const float min_temp = 0;
+const float max_temp = 45;
+const float min_soc = 20;
+const float max_soc = 80;
+const float min_CR = 0.0;
+const float max_CR = 0.8;
+
+//tolerance levels
+const float tolerance_level = 5;
+const float min_tol_temp ;
+const float max_tol_temp ;
+const float min_tol_soc ;
+const float max_tol_soc;
+const float min_tol_CR ;
+const float max_tol_CR;
+
+
 int tempIsok (float temperature)
 {
      int rangecheck_temp = 0 ;
