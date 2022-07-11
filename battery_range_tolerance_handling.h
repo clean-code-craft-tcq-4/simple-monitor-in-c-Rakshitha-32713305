6d65041fd8f86dfe115bf0aebb90d8529c982a90
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 18:13:35 +0530

    Update battery_range_tolerance_handling.h

--- battery_range_tolerance_handling.h
+++ battery_range_tolerance_handling.h
@@ -1,6 +1,6 @@
 #include "batteryparameters.h"
 
-extern int rangecheck(float input,float max,float min);
-extern void calculate_tolerance (float max,float min,float max_tol,float min_tol);
-extern void check_min_warninglevel (float input,float min,float min_tol);
-extern void check_max_warninglevel (float input,float max,float max_tol);
+extern int rangecheck(float input,float l_max,float l_min);
+extern void calculate_tolerance (float l_max,float l_min,float l_max_tol,float l_min_tol);
+extern void check_min_warninglevel (float input,float l_min,float l_min_tol);
+extern void check_max_warninglevel (float input,float l_max,float l_max_tol);
