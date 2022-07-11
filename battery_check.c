c1d7de57bf3da59b5bb7c4e08fe707145b064ba5
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-07-11 20:27:51 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -2,34 +2,43 @@
 #include "battery_check.h"
 #include "parameters_incommonunits.h"
 
+
 int is_batteryparam_ok (int ret_temp,int ret_soc,int ret_CR)
 {
      return (ret_temp && ret_soc && ret_CR);   
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate)
+void update_batterystatus(int ret)
 {
-    int ret = 0;
-    int ret_temp = 0;
-    int ret_soc  = 0;
-    int ret_CR   = 0;
-        
-    tempInCelcius(&temperature);
-         
-    ret_temp = tempIsok (temperature);
-    ret_soc  = socIsok (soc);
-    ret_CR   = chargeRateIsok (chargeRate);
-    
-    ret = is_batteryparam_ok(ret_temp,ret_soc,ret_CR); 
-    
-    if(ret == 1)
+   if(ret == 1)
     {      
        printf("BATTERY IS OK\n");
     }
     else
     {
        printf("BATTERY IS NOT OK\n");
-    }
+    }     
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate,bool conversion)
+{
+    int ret = 0;
+    int ret_temp = 0;
+    int ret_soc  = 0;
+    int ret_CR   = 0;
+    boolean convert = FALSE;
+
+  if (convert == TRUE)
+  {
+    tempInCelcius(&temperature);
+  }     
+    ret_temp = tempIsok (temperature);
+    ret_soc  = socIsok (soc);
+    ret_CR   = chargeRateIsok (chargeRate);
+    
+    ret = is_batteryparam_ok(ret_temp,ret_soc,ret_CR); 
+    update_batterystatus(ret);
+    
      
   return ret;
 
